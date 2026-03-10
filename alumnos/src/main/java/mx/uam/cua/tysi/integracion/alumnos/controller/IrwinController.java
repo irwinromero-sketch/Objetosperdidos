a426d08e2064dc04aae220ae83390d8257d793e5
Irwin <irwin.romero@cua.uam.mx>
2026-03-10 12:02:48 -0600

    descripcion del cambio

---
+++ alumnos/src/main/java/mx/uam/cua/tysi/integracion/alumnos/controller/IrwinController.java
@@ -0,0 +1,20 @@
+package mx.uam.cua.tysi.integracion.alumnos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/irwin")
+public class IrwinController {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "Hola, soy Irwin!";
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        return "Controller de Irwin funcionando correctamente";
+    }
+}
